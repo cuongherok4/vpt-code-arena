dd29880d4dd4631044680b6c8334fa622d682d6a
cuongherok4 <cuongherok4@gmail.com>
2026-07-14 02:57:45 +0700

    feat(backend): init spring boot with flyway migration and dependencies

---
+++ backend/src/main/java/com/vpt/arena/VptArenaApplication.java
@@ -0,0 +1,13 @@
+package com.vpt.arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VptArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VptArenaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vpt/arena/VptArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpt.arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VptArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
